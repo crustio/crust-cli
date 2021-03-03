492e0144aa6bc727411be15bc2dd0eaf8b33ddc8
Zikun Fan <zikunvan@gmail.com>
2021-03-03 12:11:30 +0800

    Add pin logic

--- src/pin.js
+++ src/pin.js
@@ -1,6 +1,29 @@
+const fs = require('fs');
+const createClient = require('ipfs-http-client');
+
 module.exports = { 
-    default: (cid) => {
-        // TODO: Pin to local ipfs, error with ipfs not running or shit happends
-        console.log(`Try to pin with ${cid}`);
+    default: async (path) => {
+        try {
+            // 1. Check local ipfs is alive
+            const client = createClient();
+
+            // 2. Check legality of path
+            if (!fs.existsSync(path)) {
+                console.error(`File/directory is not exists: ${path}`);
+                return;
+            }
+
+            // 3. Pin it
+            const { cid } = await client.add(path);
+
+            // 4. Check local pin
+            if (cid) {
+                console.log(`Pin success: ${cid}`);
+            } else {
+                console.error(`Pin failed, please try it again`);
+            }
+        } catch (e) {
+            console.error(`IPFS is offline, please start it over, error detail: ${e}`);
+        }
     }
 }
