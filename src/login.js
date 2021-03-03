8317ac232c34cd4d75a5005ed48783ebb01cbb21
Zikun Fan <zikunvan@gmail.com>
2021-03-03 12:03:26 +0800

    Add login logic

--- src/login.js
+++ src/login.js
@@ -1,6 +1,24 @@
+const fs = require('fs');
+
 module.exports = {
-    default: (seeds) => {
-        // TODO: store seeds locally
-        console.log(`Save ${seeds} locally`);
+    default: async (seeds) => {
+        // 1. Judge legality of the seeds
+        const seedsVec = seeds.split(' ');
+        if (seedsVec.length !== 12) {
+            console.error('Seeds illegal, check it again');
+            return;
+        }
+        
+        // 2. Write into local file
+        fs.writeFileSync('./seeds', seeds);
+        
+        // 3. Read it and compare with input seeds
+        const seedsLocal = fs.readFileSync('./seeds', 'utf8');
+
+        if (seedsLocal === seeds) {
+            console.log('Login success!');
+        } else {
+            console.error('Save seeds error, do it again.');
+        }
     }
 }
